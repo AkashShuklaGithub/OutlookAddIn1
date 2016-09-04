e52328805a3bda5a8f905bc3796406d76aeed6d8
AkashShuklaGithub <akashshukla.mail@gmail.com>
2016-09-04 17:45:17 +0530

    Add project files.

---
+++ OutlookAddIn1/FormRegion.Designer.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace OutlookAddIn1
+{
+
+    [System.ComponentModel.ToolboxItemAttribute(false)]
+    partial class FormRegion : Microsoft.Office.Tools.Outlook.FormRegionBase
+    {
+        public FormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            : base(Globals.Factory, formRegion)
+        {
+            //fill the data
+            this.myFillData();
+            //initialize the components
+            this.InitializeComponent();
+            //fill the comboboxes
+            this.myMethod();
+        }
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ComboBox siteComboBox;
+        public ComboBox floorComboBox;
+        public Label label1;
+        public Label label2;
+        public Panel panel1;
+        public static System.Collections.Generic.List<OutlookAddIn1.Site> siteList;
+        public static System.Collections.Generic.List<OutlookAddIn1.Floor> floorList;
+        public static System.Collections.Generic.List<OutlookAddIn1.Room> roomList;
+        public static List<Button> listButton;
+        public static listModel test;
+
+        private void myFillData()
+        {
+            //deserialize the text file to populate entries
+
+            System.IO.StreamReader streamReader;
+            //to do
+            //change the hard coded path to a dynamic runtime path
+            string filePath = @"C:\Users\akash2\documents\visual studio 2015\Projects\OutlookAddIn1\OutlookAddIn1\TextFile1.txt";
+
+            using (System.IO.FileStream fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Open))
+            {
+                using (streamReader = new System.IO.StreamReader(fileStream))
+                {
+                    test = Newtonsoft.Json.JsonConvert.DeserializeObject<listModel>(streamReader.ReadToEnd());
+
+                    siteList = new System.Collections.Generic.List<OutlookAddIn1.Site>();
+                    for (int i = 0; i < test.site.Count; i++)
+                    {
+                        siteList.Add(test.site[i]);
+                    }
+                }
+            }
+            streamReader.Close();
+        }
+
+        private void myMethod()
+        {
+
+           //site name list
+            //populate data
+            List<string> siteNameList = new List<string>();
+            for (int i = 0; i < siteList.Count; i++)
+            {
+                siteNameList.Add(siteList[i].siteName);
+            }
+            //set the datasource of siteCombobox as siteList
+            this.siteComboBox.DataSource = siteNameList;
+            //in case the selection of site changes
+            this.siteComboBox.SelectedIndexChanged += SiteComboBox_SelectedIndexChanged;
+
+
+           //floor name list
+            //populate data
+            floorList = new List<Floor>();
+            for (int i = 0; i < test.site[0].floor.Count; i++)
+            {
+                floorList.Add(test.site[0].floor[i]);
+            }
+            //populate combobox
+            List<string> floorNameList = new List<string>();
+            for (int i = 0; i < floorList.Count; i++)
+            {
+                floorNameList.Add(floorList[i].floorName);
+            }
+            //set datasource of floor combobox as floor names list
+            floorComboBox.DataSource = floorNameList;
+            //in case the selection of floor changes
+            this.floorComboBox.SelectedIndexChanged += FloorComboBox_SelectedIndexChanged;
+
+            //room name list
+            //populate data
+            roomList = new List<Room>();
+            int t = test.site[0].floor[0].room.Count;
+            for (int i = 0; i < t; i++)
+            {
+                roomList.Add(test.site[0].floor[0].room[i]);
+            }
+            //clear the buttons
+            panel1.Controls.Clear();
+
+            //create button list
+            listButton = new List<System.Windows.Forms.Button>();
+
+            for (int i = 0; i < t; i++)
+            {
+                listButton.Add(new System.Windows.Forms.Button());
+            }
+
+
+
+            //foreach button
+            for (int index = 0; index < t; index++)
+            {
+                panel1.Controls.Add(listButton[index]);
+                //populate buttons
+                int scale = 2;
+                listButton[index].Location = new System.Drawing.Point(roomList[index].locationX * scale, roomList[index].locationY * scale);
+                listButton[index].Name = "button" + index;
+                listButton[index].Size = new System.Drawing.Size(roomList[index].sizeX * scale, roomList[index].sizeY * scale);
+                listButton[index].Tag = roomList[index].tag;
+                listButton[index].Click += new System.EventHandler(OutlookAddIn1.ThisAddIn.button1_Click);
+                listButton[index].BackColor = System.Drawing.Color.LightYellow;
+                listButton[index].TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                listButton[index].Text = roomList[index].roomName;
+                listButton[index].TextChanged += new EventHandler(ThisAddIn.decideButtonColor);
+
+                label3.Text = "click here";
+            }
+
+        }
+
+        private void FloorComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            //room name list
+            //populate data
+            roomList = new List<Room>();
+            int siteComboBoxSelectedIndex=siteComboBox.SelectedIndex;
+            int floorComboxSelectedIndex = floorComboBox.SelectedIndex;
+            int t = test.site[siteComboBoxSelectedIndex].floor[floorComboxSelectedIndex].room.Count;
+            for (int i = 0; i < t; i++)
+            {
+                roomList.Add(test.site[siteComboBoxSelectedIndex].floor[floorComboxSelectedIndex].room[i]);
+            }
+            //clear the buttons
+            panel1.Controls.Clear();
+
+            //create button list
+            listButton = new List<System.Windows.Forms.Button>();
+            
+            for (int i = 0; i < t; i++)
+            {
+                listButton.Add(new System.Windows.Forms.Button());
+            }
+            //foreach button
+            for (int i = 0; i < t; i++)
+            {
+                panel1.Controls.Add(listButton[i]);
+                //populate buttons
+                int scale = 2;
+                listButton[i].Location = new System.Drawing.Point(roomList[i].locationX * scale, roomList[i].locationY * scale);
+                listButton[i].Name = "button" + i;
+                listButton[i].Size = new System.Drawing.Size(roomList[i].sizeX * scale, roomList[i].sizeY * scale);
+                listButton[i].Tag = roomList[i].tag;
+                listButton[i].Click += new System.EventHandler(OutlookAddIn1.ThisAddIn.button1_Click);
+                listButton[i].BackColor = System.Drawing.Color.LightYellow;
+                listButton[i].TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                listButton[i].Text = roomList[i].roomName;
+                listButton[i].TextChanged += new EventHandler(ThisAddIn.decideButtonColor);
+            }
+
+        }
+
+        private void SiteComboBox_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            //repopulate the floor name list
+            int newSelectedIndex = 0;
+            newSelectedIndex = siteComboBox.SelectedIndex;
+
+            //floor name list
+            //populate data
+            floorList.Clear();
+            for (int i = 0; i < test.site[newSelectedIndex].floor.Count; i++)
+            {
+                floorList.Add(test.site[newSelectedIndex].floor[i]);
+            }
+            //populate combobox
+            List<string> floorNameList = new List<string>();
+            for (int i = 0; i < floorList.Count; i++)
+            {
+                floorNameList.Add(floorList[i].floorName);
+            }
+            //set datasource of floor combobox as floor names list
+            this.floorComboBox.DataSource = floorNameList;
+        }
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.siteComboBox = new System.Windows.Forms.ComboBox();
+            this.floorComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // siteComboBox
+            // 
+            this.siteComboBox.BackColor = System.Drawing.Color.GhostWhite;
+            this.siteComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.siteComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.siteComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siteComboBox.Location = new System.Drawing.Point(72, 195);
+            this.siteComboBox.Name = "siteComboBox";
+            this.siteComboBox.Size = new System.Drawing.Size(121, 24);
+            this.siteComboBox.TabIndex = 0;
+            // 
+            // floorComboBox
+            // 
+            this.floorComboBox.BackColor = System.Drawing.Color.GhostWhite;
+            this.floorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.floorComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.floorComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.floorComboBox.Location = new System.Drawing.Point(72, 312);
+            this.floorComboBox.Name = "floorComboBox";
+            this.floorComboBox.Size = new System.Drawing.Size(121, 24);
+            this.floorComboBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 198);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ":Site";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 315);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ":Floor";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.Location = new System.Drawing.Point(199, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1316, 740);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 408);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Load Free/Busy";
+            // 
+            // FormRegion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.floorComboBox);
+            this.Controls.Add(this.siteComboBox);
+            this.Name = "FormRegion";
+            this.Size = new System.Drawing.Size(1405, 756);
+            this.FormRegionShowing += new System.EventHandler(this.FormRegion_FormRegionShowing);
+            this.FormRegionClosed += new System.EventHandler(this.FormRegion_FormRegionClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        public static int label3ClickLastIndex = 0;
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+            if (label3ClickLastIndex <= listButton.Count)
+            {
+                listButton[label3ClickLastIndex].Text = listButton[label3ClickLastIndex].Text + ";";
+                label3ClickLastIndex++;
+            }
+        }
+
+
+
+        #endregion
+
+        #region Form Region Designer generated code
+
+        private static void InitializeManifest(Microsoft.Office.Tools.Outlook.FormRegionManifest manifest, Microsoft.Office.Tools.Outlook.Factory factory)
+        {
+            manifest.FormRegionName = "BookRoom";
+            manifest.ShowReadingPane = false;
+
+        }
+
+        #endregion
+
+        private Label label3;
+
+        public partial class FormRegionFactory : Microsoft.Office.Tools.Outlook.IFormRegionFactory
+        {
+            public event Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler FormRegionInitializing;
+
+            private Microsoft.Office.Tools.Outlook.FormRegionManifest _Manifest;
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public FormRegionFactory()
+            {
+                this._Manifest = Globals.Factory.CreateFormRegionManifest();
+                FormRegion.InitializeManifest(this._Manifest, Globals.Factory);
+                this.FormRegionInitializing += new Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler(this.FormRegionFactory_FormRegionInitializing);
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public Microsoft.Office.Tools.Outlook.FormRegionManifest Manifest
+            {
+                get
+                {
+                    return this._Manifest;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.IFormRegion Microsoft.Office.Tools.Outlook.IFormRegionFactory.CreateFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            {
+                FormRegion form = new FormRegion(formRegion);
+                form.Factory = this;
+                return form;
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            byte[] Microsoft.Office.Tools.Outlook.IFormRegionFactory.GetFormRegionStorage(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                throw new System.NotSupportedException();
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            bool Microsoft.Office.Tools.Outlook.IFormRegionFactory.IsDisplayedForItem(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                if (this.FormRegionInitializing != null)
+                {
+                    Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs cancelArgs = Globals.Factory.CreateFormRegionInitializingEventArgs(outlookItem, formRegionMode, formRegionSize, false);
+                    this.FormRegionInitializing(this, cancelArgs);
+                    return !cancelArgs.Cancel;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.FormRegionKindConstants Microsoft.Office.Tools.Outlook.IFormRegionFactory.Kind
+            {
+                get
+                {
+                    return Microsoft.Office.Tools.Outlook.FormRegionKindConstants.WindowsForms;
+                }
+            }
+        }
+    }
+
+
+    partial class WindowFormRegionCollection
+    {
+        internal FormRegion FormRegion
+        {
+            get
+            {
+                foreach (var item in this)
+                {
+                    if (item.GetType() == typeof(FormRegion))
+                        return (FormRegion)item;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ OutlookAddIn1/ThisAddIn.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading;
+
+namespace OutlookAddIn1
+{
+    public partial class ThisAddIn
+    {
+        //create a new inspector for memory-handling
+        Outlook.Inspectors inspectors;
+
+        public static Outlook.AppointmentItem appointmentItem;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+
+            //attach an event handler to the inspector
+            //the 'Application' class represents the current instance of Outlook
+            //'inspector' represents the inspector window of the new mail message
+            inspectors = this.Application.Inspectors;
+            inspectors.NewInspector += Inspectors_NewInspector;
+
+        }
+
+        private void Inspectors_NewInspector(Outlook.Inspector Inspector)
+        {
+            //example: when the user creates a new meeting item, the subject and body is populated by code
+            appointmentItem = Inspector.CurrentItem as Outlook.AppointmentItem;
+            if(appointmentItem != null)
+            {
+                if(appointmentItem.EntryID==null)
+                {
+                    appointmentItem.Subject = "This subject was added via code";
+                }
+            }
+
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+
+        public static void button1_Click(object sender, EventArgs e)
+        {
+            var btn = (System.Windows.Forms.Button)sender;
+            if (btn.Tag.ToString() != "")
+            {
+                appointmentItem.Recipients.Add(btn.Tag.ToString());
+                btn.BackColor = System.Drawing.Color.DarkOliveGreen;
+            }
+        }
+
+        public static void decideButtonColor(object sender, EventArgs e)
+        {
+            var btn = (System.Windows.Forms.Button)sender;
+            //BackgroundWorker bw = new BackgroundWorker();
+            string buttonTag = btn.Tag.ToString();
+
+
+            //bw.DoWork += new DoWorkEventHandler(
+            //delegate (object o, DoWorkEventArgs args)
+            //{
+            if (btn.Tag.ToString() != "")
+            {
+                appointmentItem.Recipients.Add(btn.Tag.ToString());
+
+                int startIndex;
+                string freeBusy;
+
+                int startHour = appointmentItem.StartInStartTimeZone.Hour;
+                int startMinute = appointmentItem.StartInStartTimeZone.Minute;
+                if (startMinute < 30)
+                    startIndex = startHour * 2;
+                else
+                    startIndex = startHour * 2 + 1;
+
+                //for(int i=1;i<appointmentItem.Recipients.Count;i++)
+                //{
+                //  if(appointmentItem.Recipients[i].Name==buttonTag)
+                //{
+                //freeBusy = appointmentItem.Recipients[appointmentItem.Recipients.Count].FreeBusy(appointmentItem.StartInStartTimeZone.Date, 30, false).Substring(0, 48);
+                freeBusy = appointmentItem.Recipients[appointmentItem.Recipients.Count].FreeBusy(appointmentItem.StartInStartTimeZone.Date, 30, false);
+                if (freeBusy != null)
+                {
+                    if (freeBusy[startIndex] == '0')
+                        btn.BackColor = System.Drawing.Color.LightGreen;//free
+                    else
+                        btn.BackColor = System.Drawing.Color.OrangeRed;//busy
+                }
+
+                appointmentItem.Recipients.Remove(appointmentItem.Recipients.Count);
+            }
+                //});
+
+            //bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
+            //    delegate (object o, RunWorkerCompletedEventArgs args)
+            //    {
+            //    });
+
+            //bw.RunWorkerAsync();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+    
+    //define the list model
+    public class listModel
+    {
+        public List<Site> site { get; set; }
+    }
+
+    public class Site
+    {
+        public string siteName { get; set; }
+        public List<Floor> floor { get; set; }
+    }
+
+    public class Floor
+    {
+        public string floorName { get; set; }
+        public List<Room> room { get; set; }
+    }
+
+    public class Room
+    {
+        public string roomId { get; set; }
+        public string roomName { get; set; }
+        public int locationX { get; set; }
+        public int locationY { get; set; }
+        public int sizeX { get; set; }
+        public int sizeY { get; set; }
+        public string tag { get; set; }
+    }
+
+
+}
